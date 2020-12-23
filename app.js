7235c9069179f4fe72fd8e7207acdb49aac2cada
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2020-12-24 02:19:59 +0530

    added passport js backend only registration

--- app.js
+++ app.js
@@ -1,103 +1,192 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const cookieParser = require('cookie-parser');
-const admin = require('firebase-admin');
-const csrf  = require('csurf');
+const session = require('express-session');
+const passport = require('passport');
+const validator = require('validator');
+const {v4: uuidv4} = require('uuid');
 
-const serviceAccount = require("./serviceAccountKey.json");
+require('dotenv').config();
 
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount)
-});
-
-const csrfMiddleware = csrf({ cookie: true});
-const db = admin.firestore();
+const mongoose = require('./db/mongoose');
+const User = require('./schema/userSchema');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-//app.engine("html", require("ejs").renderFile);
 
-app.use(express.static('static'));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(csrfMiddleware);
+app.use(express.static('public'));
 
-app.all("*", (req, res, next) => {
-	res.cookie("XSRF-TOKEN", req.csrfToken());
-	next();
-})
+app.use(session({
+	secret: process.env.SECRET_KEY,
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+	User.findById(id, function(err, user) {
+		done(err, user);
+	});
+});
 
 app.get("/", (req,res) => {
+	
 	res.send('Hello World!');
+	
 });
 
-app.get("/login", (req, res) => {
-	res.render('login.ejs');
+app.get("/home", (req,res) => {
+	
+	if(req.isAuthenticated())
+	{
+		res.send("home");
+	}
+	else
+	{
+		res.redirect("/login");
+	}
+	
 });
 
-app.get('/signup', (req, res) => {
-	res.render('signup.ejs');
+app.get("/login", (req,res) => {
+	
+	if(req.isAuthenticated())
+	{
+		res.redirect("home");
+	}
+	else
+	{
+		res.render("login");
+	}
+	
 });
 
-var data = {};
+app.post('/login', (req,res) => {
+	
+	if(req.isAuthenticated())
+	{
+		res.redirect("home");
+	}
+	else
+	{
+		res.render("login");
+	}
+});
 
-app.get('/home', (req, res) => {
-	const sessionCookie = req.cookies.session || "";
+app.get("/signup", (req,res) => {
+	
+	if(req.isAuthenticated())
+	{
+		res.redirect("/home");
+	}
+	else
+	{
+		res.render("signup");
+	}
+	
+});
 
-	admin
-		.auth()
-		.verifySessionCookie(sessionCookie, true)
-		.then(async (user) => {
-			console.log(user.uid);
-			db.collection('users').where('userId', '==', user.uid)
-				.get()
-				.then((querySnapshot) => {
-					console.log(querySnapshot);
-					querySnapshot.forEach((doc) => {
-						console.log(doc.id, " => ", doc.data());
-						data = doc.data();
-						console.log(data.teamName);
+app.post('/signup', (req,res) => {
+	
+	if(req.isAuthenticated())
+	{
+		res.status(404);
+	}
+	else
+	{
+		let patt = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
+		if(patt.test(req.body.password))
+		{
+			User.register(
+				{
+					username: req.body.username,
+					teamName: req.body.team,
+					verified: false,
+					verificationUrl: uuidv4(),
+					pwChangeUrl: '0'
+				}, req.body.password, (err, user) => {
+				if(err)
+				{
+					if(err.name === "UserExistsError")
+					{
+						User.where({username: req.body.username}).findOne((err,user) => {
+							if(err)
+							{
+								console.log(err)
+								res.send({message: "Server Error"});
+							}
+							else
+							{
+								if(user.verified)
+								{
+									res.send({message: 'User already registered!'})
+								}
+								else
+								{
+									User.deleteOne({username: req.body.username}, delErr => {
+										if(delErr)
+										{
+											console.log(delErr);
+											res.send({message: "Server Error"});
+										}
+										else
+										{
+											res.send({message: "one-more"});
+										}
+									})
+								}
+							}
+						})
+					}
+					else
+					{
+						if(err.name === 'MongoError' && err.code === 11000)
+						{
+							res.send({message: "Team Name already taken"});
+						}
+						else if(err.errors.username !== undefined && err.errors.username.name === 'ValidatorError')
+						{
+							res.send({message: err.errors.username.message});
+						}
+						else if(err.errors.teamName !== undefined && err.errors.teamName.name === 'ValidatorError')
+						{
+							res.send({message: 'Team Name should contain minimum 4 characters!'});
+						}
+						else
+						{
+							console.log(JSON.stringify(err))
+							res.send({message: "Server Error"});
+						}
+						
+					}
+				}
+				else
+				{
+					passport.authenticate("local")(req,res,() => {
+						res.send({message: 'done'});
 					});
-					res.render('home.ejs', {"team": data.teamName});
-				})
-				.catch((err) => {
-					console.log("Error getting data");
-					res.render('home.ejs');
-				})
-		})
-		.catch((err) => {
-			res.redirect('/login')
-		});
+				}
+			});
+		}
+		else{
+			res.send({message: 'Password doesn\'t satisfy the conditions!'})
+		}
+	}
+	
 });
 
-app.post('/sessionLogin', (req, res) => {
-	const idToken = req.body.idToken.toString();
 
-	const expiresIn = 60 * 60 * 2 * 1000;
-
-	admin
-		.auth()
-		.createSessionCookie(idToken, {expiresIn})
-		.then(
-			(sessionCookie) => {
-				const options = {maxAge: expiresIn, httpOnly: true};
-				res.cookie("session", sessionCookie, options);
-				res.end(JSON.stringify({ status: "success"}));
-			},
-			(error) => {
-				res.status(401).send("UNAUTHORIZED REQUEST");
-			}
-		);
-});
-
-app.get('/sessionLogout', (req, res) => {
-	res.clearCookie("session");
-	res.redirect('/login');
-});
-
-app.listen(4000, () => {
-	console.log('Listening on port 4000');
+app.listen(3000, () => {
+	console.log('Listening to port 3000');
 });
---
+++ schema/userSchema.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const passportLocalMongoose = require('passport-local-mongoose');
+const findOrCreate = require('mongoose-findorcreate');
+
+const userSchema = new mongoose.Schema({
+	username: {
+        type: String,
+        required: [true, 'Email Address Required'],
+        trim: true,
+        lowercase: [true, 'Invalid Email Address'],
+        unique: [true, 'You have already Registered!'],
+        validate: {
+            validator: (value) => {
+                return validator.isEmail(value);
+            },
+            message: 'Invalid email address provided'
+        }
+    },
+	password: {
+        type: String
+    },
+    teamName: {
+        type: String,
+        required: [true, 'Team Name Required'],
+        unique: [true, 'Team Name already taken!'],
+        minLength: 4
+    },
+    verified: {
+        type: Boolean,
+        required: true
+    },
+    verificationUrl: {
+        type: String,
+        required: true
+    },
+    pwChangeUrl: {
+        type: String,
+        required: true
+    }
+    
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model('User', userSchema);
+
+module.exports = User;
