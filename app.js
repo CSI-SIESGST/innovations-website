3ca8efa6fb5b097086bbdebaaaf2daed8c19b22d
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2021-02-01 17:24:15 +0530

    update security key from env

--- app.js
+++ app.js
@@ -16,7 +16,18 @@ require('./db/mongoose');
 const User = require('./schema/userSchema');
 const Chat = require('./schema/chatSchema');
 const Broadcast = require('./schema/broadcastSchema');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = {
+	type: 'service_account',
+	project_id: process.env.PROJECT_ID,
+	private_key_id: process.env.PRIVATE_KEY_ID,
+	private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+	client_email: process.env.CLIENT_EMAIL,
+	client_id: process.env.CLIENT_ID,
+	auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+	token_uri: 'https://oauth2.googleapis.com/token',
+	auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+	client_x509_cert_url: process.env.CERT_URL
+};
 
 //initialize firebase app
 admin.initializeApp({
